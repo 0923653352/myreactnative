b4d3c1e08873eabfb1241d68c081f194afd39c11
Nawathpolkaw <nawathpolkaw@gmail.com>
2022-02-03 17:34:52 +0700

    week11

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -5,6 +5,8 @@ import PokemonTab from "./PokemonTab";
 import Book from "../screens/week9/Book";
 import BookDetail from "../screens/week9/BookDetail";
 import BookForm from "../screens/week9/BookForm";
+import Location from "../screens/week11/Location";
+import LocationQuiz from "../screens/week11/LocationQuiz";
 const Stack = createStackNavigator();
 
 export default function RootStack() {
@@ -27,6 +29,8 @@ export default function RootStack() {
         options={{ title: "Book Detail" }}
       />
       <Stack.Screen name="BookForm" component={BookForm} />
+      <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
     </Stack.Navigator>
   );
 }
