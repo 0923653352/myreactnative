53a18f666b104a6f57733525414945000522f5e9
Nawathpolkaw <79831887+0923653352@users.noreply.github.com>
2022-02-25 14:29:22 +0700

    week14

--- services/BookLaravel.js
+++ services/BookLaravel.js
@@ -1,8 +1,11 @@
 // const url_endpoint = "http://localhost/mylaravel8/public/api/book";
-const url_endpoint = "http://192.168.1.3/mylaravel8/public/api/book";
+// const url_endpoint = "http://192.168.1.3/mylaravel8/public/api/book";
+import { APP_URL } from "@env";
+const url_endpoint = APP_URL+"/api/book";
 import * as mime from "mime";
 
 const getItems = async () => {
+  console.log(url_endpoint,APP_URL);
   try {
     
     let response = await fetch(url_endpoint);
@@ -28,7 +31,7 @@ const storeItem = async (item) => {
        //CREATE FORM DATA
        let data = new FormData();
        for (let key in item) {
-         if (key == "image") continue;
+        //  if (key == "image") continue;
          data.append(key, item[key]);
        }
        if (item.image.split(":")[0] == "file") {
